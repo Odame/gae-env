f7894e0979784d55531bc05154784f0387270aa4
Prince Odame <opodame@gmail.com>
2018-10-05 04:38:54 +0000

    configured readme as package long_description on pypi

--- setup.py
+++ setup.py
@@ -1,21 +1,18 @@
 from setuptools import setup
 
-# the rst content from this function will be rendered as the lib 'homepage' on pypi
-
 
 def readme():
-    with open('README.rst') as readme_file:
+    """ Read the content of the README.md file as the long description """
+    with open('README.md') as readme_file:
         return readme_file.read()
 
 
 setup(
     name='gae_env',
     version='0.1.0',
-    description='Google Appengine environment variables stored in Cloud Datastore',
-    long_description='Google Appengine environment variables stored in Cloud Datastore.' +
-    'This enables easy access to edit variables in Developers Console, ' +
-    'and prevents bad practice of storing environment variables in code ' +
-    '(app.yaml or secrets.json files)',
+    description='Google Appengine environment variables stored in Cloud Datastore (and/or system environment variables)',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
@@ -28,5 +25,5 @@ setup(
     author_email='opodame@gmail.com',
     license='MIT',
     packages=['gae_env'],
-    zip_safe=False,
+    zip_safe=False
 )
