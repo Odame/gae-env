f180502cffedce8916fbd52beb359a7a9f9bdbf5
Prince Odame <opodame@gmail.com>
2018-10-05 06:45:59 +0000

    updated development status to beta in setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description=readme(),
     long_description_content_type="text/markdown",
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
         'Topic :: Software Development :: Libraries',
